9c49ab321ca1d6613ca162c0bba922dda163c321
JEFFWIN10\Jeff <jlt212000@yahoo.com>
2019-06-19 21:31:36 +0800

    adding click handler

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ const Pet = (props) => {
 }
 class App extends React.Component
 {
+    handleTitleClick(){
+        alert("you clicked the title");
+    }
+
     render()
     {
         return React.createElement("div",{}, [
-            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement("h1", { onClick:this.handleTitleClick }, "Adopt Me!"),
             React.createElement(Pet,{name:'Luna', animal: 'dog', breed:'Havanese'}),
             React.createElement(Pet,{name:'Pepper', animal: 'bird', breed:'Cockatiel'}),
             React.createElement(Pet,{name:'Doink', animal: 'cat', breed:'Mixed'}),
