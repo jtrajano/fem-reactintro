6c6e48ae6101481c9d2e50a55a330950985e3346
JEFFWIN10\Jeff <jlt212000@yahoo.com>
2019-06-23 00:25:09 +0800

    search control

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import SearchParams from "./SearchParams";
 
-class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
+// class App extends React.Component {
+//   handleTitleClick() {
+//     alert("you clicked the title");
+//   }
 
-  render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "bird",
-        breed: "Cockatiel"
-      }),
+//   // render() {
+//   //   return React.createElement("div", {}, [
+//   //     React.createElement(
+//   //       "h1",
+//   //       { onClick: this.handleTitleClick },
+//   //       "Adopt Me!"
+//   //     ),
+//   //     React.createElement(Pet, {
+//   //       name: "Luna",
+//   //       animal: "dog",
+//   //       breed: "Havanese"
+//   //     }),
+//   //     React.createElement(Pet, {
+//   //       name: "Pepper",
+//   //       animal: "bird",
+//   //       breed: "Cockatiel"
+//   //     }),
 
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "Mixed"
-      })
-    ]);
-  }
-}
+//   //     React.createElement(Pet, {
+//   //       name: "Doink",
+//   //       animal: "cat",
+//   //       breed: "Mixed"
+//   //     })
+//   //   ]);
+//   // }
+
+// }
+const App = () => {
+  return (
+    <div>
+      <h1 id="somthing-important"> Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
 
 render(React.createElement(App), document.getElementById("root"));
